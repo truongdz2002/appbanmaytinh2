987a815769bdb57c47843a28c5740ba316f78f7d
Phan Cong Long <pcl2k2@gmail.com>
2022-03-13 21:23:42 +0700

    Adding RegisterActivity and Validate Form RegisterActivity

--- app/src/main/java/com/example/appbanmaytinh/MainActivitylogin.java
+++ app/src/main/java/com/example/appbanmaytinh/MainActivitylogin.java
@@ -7,17 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.appbanmaytinh.Package.RegisterActivity;
+
 public class MainActivitylogin extends AppCompatActivity {
-Button login;
+    Button login , btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activitylogin);
-        login=findViewById(R.id.login);
+        login = findViewById(R.id.login);
+        btnRegister = findViewById(R.id.Register);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivitylogin.this,MainActivity.class);
+                Intent intent = new Intent(MainActivitylogin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivitylogin.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/appbanmaytinh/Package/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.appbanmaytinh.Package;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.appbanmaytinh.MainActivity;
+import com.example.appbanmaytinh.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    Button btnRegister;
+    EditText gmail, password, rePassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        queryItem();
+
+        gmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkValidateForm();
+
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkValidateForm();
+            }
+        });
+
+        rePassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkValidateForm();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void queryItem() {
+        btnRegister = findViewById(R.id.btnRegisterRegister);
+        gmail = findViewById(R.id.edGmailRegister);
+        password = findViewById(R.id.edPasswordRegister);
+        rePassword = findViewById(R.id.edRePasswordRegigter);
+    }
+
+    private void checkValidateForm() {
+        if (gmail.getText().toString().trim().matches(emailPattern) && password.getText().toString().trim().equals(rePassword.getText().toString().trim()) && !password.getText().toString().trim().equals("") && !rePassword.getText().toString().trim().equals("")) {
+            btnRegister.setEnabled(true);
+        } else {
+            btnRegister.setEnabled(false);
+        }
+    }
+}
